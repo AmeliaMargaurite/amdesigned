54e525a15c57bf8cffd804a18a897e4a7f257d2c
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2023-03-09 13:33:27 +0100

    fixes to load article to work live and local, start ui changes article page

--- src/article/load_article.php
+++ src/article/load_article.php
@@ -5,18 +5,26 @@ include_once('../php/config.php');
 use GuzzleHttp\Client;
 
 $article = null;
-$uri_parts = explode('uri=', $_SERVER["QUERY_STRING"]);
+$article_slug = null;
+if (IS_LIVE) {
+  $uri_parts = explode('uri=', $_SERVER["QUERY_STRING"]);
+  if (count($uri_parts) > 0 && !empty($uri_parts[1])) {
+    $article_slug = '/' . htmlspecialchars($uri_parts[1]);
+  }
+} else {
+  $article_slug = htmlspecialchars($_SERVER['PATH_INFO']);
 
-if (count($uri_parts) > 0 && !empty($uri_parts[1])) {
-  $article_slug = $uri_parts[1];
+}
 
+if ($article_slug) {
   $base_url = $_SERVER['HTTP_HOST'] === 'localhost:3001' ? 'http://127.0.0.1:8000/' : 'https://www.access.amdesigned.com.au/';
 
   $img_url = $base_url . 'storage/gallery/';
   $api_addon = 'api/articles';
+  $url = $base_url . $api_addon . $article_slug;
 
   $client = new Client();
-  $res = $client->get($base_url . $api_addon . '/' . $article_slug);
+  $res = $client->get($url);
 
   $article = json_decode($res->getBody());
 
