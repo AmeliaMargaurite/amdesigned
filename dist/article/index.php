675318b351a724040581c42b856db568bfb57af3
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2023-03-14 17:14:04 +0100

    'rebuild'

--- dist/article/index.php
+++ dist/article/index.php
@@ -16,8 +16,8 @@
     <meta name="twitter:title" content="&lt;?= $article ? $article-&gt;title : 'Whoops!' ?&gt; | AMDesigned" />
     <meta name="twitter:description" content="AMDesigned" />
     <meta name="twitter:image" content="" />
-    <script src="/js/runtime.6924b43834953277efd8.js"> </script><script src="/js/index.8.1b6d8c23c3108d19ba04.js"> </script>
-    <link href="/css/main.0866b61507137cb42228ef4136c1acb4.css" rel="stylesheet" />
+    <script src="/js/runtime.6924b43834953277efd8.js"> </script><script src="/js/index.10.a394cfe2feb1edba2c53.js"> </script>
+    <link href="/css/main.f32fedeb72942c9f5715c2261ef10120.css" rel="stylesheet" />
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.478ba290519eb28e.png" />
     <link rel="icon" type="image/png" sizes="512x512" href="/assets/img/android-chrome-512x512.7445bebac609ad72.png" />
     <link rel="icon" type="image/png" sizes="192x192" href="/assets/img/android-chrome-192x192.cf989685129b62c1.png" />
