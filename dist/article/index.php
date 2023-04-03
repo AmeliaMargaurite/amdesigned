0af8bb28dd6ca56f01a61bd7fac7b04cf8b6c9e8
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2023-04-03 16:54:05 +0200

    'rebuild'

--- dist/article/index.php
+++ dist/article/index.php
@@ -19,7 +19,7 @@
     <meta name="twitter:image"
       content="<?= $article ? $img_path . 'medium/' . $article->hero_img_filename . '.jpg' : '' ?>" />
     <script src="/js/runtime.6215ad7d9669b3713aae.js"> </script><script src="/js/common.eee5d1964e08124eab65.js"> </script>
-    <link href="/css/main.a7252d17e4bdd674e0c64c055cc65dca.css" rel="stylesheet" />
+    <link href="/css/main.6662af6dd020ae83a7223ca78a13212a.css" rel="stylesheet" />
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.478ba290519eb28e.png" />
     <link rel="icon" type="image/png" sizes="512x512" href="/assets/img/android-chrome-512x512.7445bebac609ad72.png" />
     <link rel="icon" type="image/png" sizes="192x192" href="/assets/img/android-chrome-192x192.cf989685129b62c1.png" />
