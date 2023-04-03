c806882477d449170ef587219d58f4ac9fcea2f2
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2023-04-03 16:40:27 +0200

    fix adding robots.txt back into public_html folder

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,17 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
 	mode: "production",
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.join(__dirname, "robots.txt"),
+					to: path.join(__dirname, "dist"),
+				},
+			],
+		}),
+	],
 });
