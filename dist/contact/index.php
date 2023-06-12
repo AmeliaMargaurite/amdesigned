33cb48e5b472c9201e0f4e7addaf478a703f16ae
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2023-06-12 11:35:59 +0200

    'rebuild'

--- dist/contact/index.php
+++ dist/contact/index.php
@@ -23,73 +23,13 @@
       content="Contact us now for a fast, easy website for your business. SEO and accessibility focused, your customers will love your new website" />
     <meta name="twitter:image"
       content="/assets/img/amdesigned-business-websites-australia-get-online.f6cc952806268e89.png" />
-    <script src="/js/runtime.6215ad7d9669b3713aae.js"> </script><script src="/js/common.6dfe874d4eed07968fc9.js"> </script>
-    <link href="/css/main.035a05b766661e80bfb76075a36c99ed.css" rel="stylesheet" />
+    <link href="/css/main.4ab0994b42770ab2559f2ee6a782dd0e.css" rel="stylesheet" />
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.478ba290519eb28e.png" />
     <link rel="icon" type="image/png" sizes="512x512" href="/assets/img/android-chrome-512x512.7445bebac609ad72.png" />
     <link rel="icon" type="image/png" sizes="192x192" href="/assets/img/android-chrome-192x192.cf989685129b62c1.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.fb29bbf81ec57714.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.f68b7860c31a0afb.png" />
     <link rel="manifest" href="../favicon/site.webmanifest" />
-    <script>
-      !(function (t, e) {
-        var o, n, p, r;
-        e.__SV ||
-          ((window.posthog = e),
-            (e._i = []),
-            (e.init = function (i, s, a) {
-              function g(t, e) {
-                var o = e.split(".");
-                2 == o.length && ((t = t[o[0]]), (e = o[1])),
-                  (t[e] = function () {
-                    t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
-                  });
-              }
-              ((p = t.createElement("script")).type = "text/javascript"), (p.async = !0), (p.src = s.api_host +
-                "/static/array.js"), (r = t.getElementsByTagName("script")[0]).parentNode.insertBefore(p, r);
-              var u = e;
-              for (
-                void 0 !== a ? (u = e[a] = []) : (a = "posthog"),
-                u.people = u.people || [],
-                u.toString = function (t) {
-                  var e = "posthog";
-                  return "posthog" !== a && (e += "." + a), t || (e += " (stub)"), e;
-                },
-                u.people.toString = function () {
-                  return u.toString(1) + ".people (stub)";
-                },
-                o =
-                "capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags"
-                .split(" "),
-                n = 0; n < o.length; n++
-              )
-                g(u, o[n]);
-              e._i.push([i, s, a]);
-            }),
-            (e.__SV = 1));
-      })(document, window.posthog || []);
-      const getUserId = () => {
-        let user_id;
-        if (window.localStorage.getItem("user_id")) {
-          user_id = JSON.parse(window.localStorage.getItem("user_id"));
-        } else {
-          user_id = Date.now();
-          window.localStorage.setItem("user_id", user_id);
-        }
-        return [user_id];
-      };
-      posthog.init("phc_IECNjEcCsaDAmsFD1y49ormP3Xj7Fzh6aHCqYWd6pNE", {
-        api_host: "https://eu.posthog.com",
-        loaded: (posthog) => {
-          posthog.identify(getUserId());
-          const toolbarJSON = new URLSearchParams(window.location.hash.substring(1)).get("__posthog");
-          if (toolbarJSON) {
-            posthog.loadToolbar(JSON.parse(toolbarJSON));
-          }
-        },
-        persistence: "memory",
-      });
-    </script>
   </head>
   <body class="contact"><span class="loading__wrapper" id="loading__wrapper"><span
         class="loader">Loading...</span></span>
@@ -168,6 +108,8 @@
             </form>
             <p class="warning">
               <script
+                src="/js/runtime.6215ad7d9669b3713aae.js">
+              </script><script
                 src="/js/contact_form.0a1d9470141bb46463ae.js">
               </script>
             </p>
@@ -220,5 +162,8 @@
         <li> <a href="/privacy-policy">Privacy policy</a></li>
       </ul>
     </footer>
+    <script src="/js/common.6dfe874d4eed07968fc9.js"> </script>
+    <script src="/js/lightDarkMode.0d53ad7aa4e22f193d50.js"> </script>
+    <script src="/js/posthog.d90645040e27fcfdde8e.js"></script>
   </body>
 </html>
